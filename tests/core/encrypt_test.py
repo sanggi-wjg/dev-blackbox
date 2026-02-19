000087ddffcb8e10dc0ddd95ddb5995d2e738871
Raynor <jonggu.woo@fitpet.co.kr>
2026-02-19 19:22:30 +0900

    Jira 및 Slack 사용자 데이터 암호화 로직 추가 - 사용자 이름 및 이메일 암호화 처리 로직 반영 (Jira/Slack). - `EncryptService` 테스트 케이스 확장 및 빈 문자열 처리 추가.

--- tests/core/encrypt_test.py
+++ tests/core/encrypt_test.py
@@ -14,3 +14,18 @@ def test_encrypt_service():
     decrypted_text = encrypt_service.decrypt(encrypted_text)
 
     assert decrypted_text == secret_key
+
+
+def test_encrypt_service_when_empty_secret():
+    key = "test_key"
+    pepper = "test_pepper"
+    encrypt_service = EncryptService(key, pepper)
+
+    # given
+    empty_secret_key = ""
+
+    # when
+    encrypted_text = encrypt_service.encrypt(empty_secret_key)
+    decrypted_text = encrypt_service.decrypt(encrypted_text)
+
+    assert decrypted_text == empty_secret_key
